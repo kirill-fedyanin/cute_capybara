f68ab2206257a3daa4f59256189436700318871e
mephody-bro <kirill.fedyanin@gmail.com>
2018-10-21 22:48:58 +0300

    Upload status

--- image_upload.py
+++ image_upload.py
@@ -1,3 +1,23 @@
+import requests
+
 img_name = 'https://i.ytimg.com/vi/m9UE83BCROM/maxresdefault.jpg'
 o_auth = 'AQAAAAABHv2XAAT7o2e3Q1LJnUWhuZG1vJrwlyw'
-print(o_auth)
+skill_id = '32dda7b4-b4f9-4517-8aa2-fd2853ea4385'
+# base_url = 'https://dialogs.yandex.ru/developer/skills/'
+base_url = 'https://dialogs.yandex.net/api/v1/'
+path = 'status'
+
+headers = {
+    'Authorization': f'OAuth {o_auth}'
+}
+
+r = requests.get(base_url + path, headers=headers)
+print(r)
+print(r.text)
+
+
+
+
+# for line in lines:
+#     word = line.split()[0]
+#     words.append(word)
