93e62eb967a48002b279a7c78a40cd66377e8c1b
mephody-bro <kirill.fedyanin@gmail.com>
2018-10-21 22:49:19 +0300

    Upload status

--- image_upload.py
+++ image_upload.py
@@ -5,18 +5,20 @@ o_auth = 'AQAAAAABHv2XAAT7o2e3Q1LJnUWhuZG1vJrwlyw'
 skill_id = '32dda7b4-b4f9-4517-8aa2-fd2853ea4385'
 # base_url = 'https://dialogs.yandex.ru/developer/skills/'
 base_url = 'https://dialogs.yandex.net/api/v1/'
-path = 'status'
 
 headers = {
     'Authorization': f'OAuth {o_auth}'
 }
 
-r = requests.get(base_url + path, headers=headers)
-print(r)
-print(r.text)
+
+def get_status():
+    path = 'status'
+    r = requests.get(base_url + path, headers=headers)
+    print(r)
+    print(r.text)
 
 
-
+get_status()
 
 # for line in lines:
 #     word = line.split()[0]
